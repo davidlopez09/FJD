ed42df960f13f20bea82fade21af0de699376b15
Carlos <mariotiradotovar@gmail.com>
2024-04-16 17:55:23 -0500

    cam

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -77,8 +77,8 @@ export function Contactanos() {
             <div className="map">
                 <iframe
                     className="mapa"
-                    width="80%"
-                    height="300"
+                    width="90%"
+                    height="250"
                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3943.185471982326!2d-75.890436!3d8.768614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwNDYnMDcuMCJOIDc1wrA1MycyNS42Ilc!5e0!3m2!1ses!2sco!4v1713296345732!5m2!1ses!2sco"></iframe>
             </div>
             <div className="conten">
