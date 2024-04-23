4300932ded0b4bea9d4e5a7a8e1f9dee60201ce3
Carlos <mariotiradotovar@gmail.com>
2024-04-23 14:36:44 -0500

    swiper

--- src/components/perfiles.jsx
+++ src/components/perfiles.jsx
@@ -4,87 +4,30 @@ import desarrolloproyectos from "../assets/images/perfil1.webp";
 import analisisrequerimiento from "../assets/images/perfil2.webp";
 import blogs from "../assets/images/perfil3.webp";
 import imagen from "../assets/images/perfil4.webp";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+import { FreeMode, Pagination } from "swiper/modules";
 
 export function Perfiles() {
     const perfilesSecundarios = [
-        {
-            imagen: desarrolloproyectos,
-            saludo: "¡Hola!",
-            nombre: "Soy Ana",
-            cargo: "Desarrolladora de Proyectos",
-        },
-        {
-            imagen: analisisrequerimiento,
-            saludo: "¡Hola!",
-            nombre: "Soy Carlos",
-            cargo: "Tester",
-        },
-        {
-            imagen: blogs,
-            saludo: "¡Hola!",
-            nombre: "Soy Juan",
-            cargo: "Contador",
-        },
-        {
-            imagen: imagen,
-            saludo: "¡Hola!",
-            nombre: "Soy Ada",
-            cargo: "Diseñadora",
-        },
-        {
-            imagen: imagen,
-            saludo: "¡Hola!",
-            nombre: "Soy Maria",
-            cargo: "Diseñadora",
-        },
-        {
-            imagen: analisisrequerimiento,
-            saludo: "¡Hola!",
-            nombre: "Soy Carlos",
-            cargo: "Tester",
-        },
-        {
-            imagen: blogs,
-            saludo: "¡Hola!",
-            nombre: "Soy Juan",
-            cargo: "Contador",
-        },
-        {
-            imagen: blogs,
-            saludo: "¡Hola!",
-            nombre: "Soy Luis",
-            cargo: "Programador Junior",
-        },
+        { imagen: desarrolloproyectos, nombre: "Ana", cargo: "Desarrolladora de Proyectos" },
+        { imagen: analisisrequerimiento, nombre: "Carlos", cargo: "Tester" },
+        { imagen: blogs, nombre: "Juan", cargo: "Contador" },
+        { imagen: imagen, nombre: "Ada", cargo: "Diseñadora" },
+        { imagen: imagen, nombre: "Maria", cargo: "Diseñadora" },
+        { imagen: analisisrequerimiento, nombre: "Carlos", cargo: "Tester" },
+        { imagen: blogs, nombre: "Juan", cargo: "Contador" },
+        { imagen: blogs, nombre: "Luis", cargo: "Programador Junior" },
     ];
 
     const [perfilPrincipalIndex, setPerfilPrincipalIndex] = useState(0);
-    const perfilesVisibles = 5; // Número de perfiles secundarios visibles inicialmente
-    const totalPerfiles = perfilesSecundarios.length;
+    const [perfilSecundarioSeleccionado, setPerfilSecundarioSeleccionado] = useState(null);
 
-    const cambiarPerfil = (direccion) => {
-        let newIndex = perfilPrincipalIndex;
-
-        if (direccion === "izquierda") {
-            newIndex = (perfilPrincipalIndex - 1 + totalPerfiles) % totalPerfiles;
-        } else {
-            newIndex = (perfilPrincipalIndex + 1) % totalPerfiles;
-        }
-
-        setPerfilPrincipalIndex(newIndex);
+    const cambiarPerfil = (index) => {
+        setPerfilPrincipalIndex(index);
     };
-
-    // Calcular los índices de los perfiles secundarios visibles alrededor del perfil principal
-    const halfVisibles = Math.floor(perfilesVisibles / 2);
-    let startIndex = perfilPrincipalIndex - halfVisibles;
-    if (startIndex < 0) {
-        startIndex += totalPerfiles; // Ajustar para manejar índices negativos
-    }
-
-    const visiblePerfiles = [];
-    for (let i = 0; i < perfilesVisibles; i++) {
-        const index = (startIndex + i) % totalPerfiles;
-        visiblePerfiles.push(perfilesSecundarios[index]);
-    }
 
     return (
         <div className="container-perfil">
@@ -95,25 +38,43 @@ export function Perfiles() {
                         <img className="img-perfil" src={perfilesSecundarios[perfilPrincipalIndex].imagen} alt="" />
                     </div>
                     <div className="desc-perfil">
-                        <h1 className="saludo">{perfilesSecundarios[perfilPrincipalIndex].saludo}</h1>
+                        <h1 className="saludo">¡Hola!</h1>
                         <h1 className="nombre">{perfilesSecundarios[perfilPrincipalIndex].nombre}</h1>
                         <p className="cargo">{perfilesSecundarios[perfilPrincipalIndex].cargo}</p>
                     </div>
                 </div>
 
                 <div className="perfil-secundario">
-                    <i
-                        className="bx bxs-chevron-left-circle icon-perfil"
-                        onClick={() => cambiarPerfil("izquierda")}></i>
-                    {visiblePerfiles.map((perfil, index) => (
-                        <div
-                            key={index}
-                            className={`sec-perfil ${index === halfVisibles ? "selected" : ""}`}
-                            onClick={() => setPerfilPrincipalIndex((startIndex + index) % totalPerfiles)}>
-                            <img className="img-sec" src={perfil.imagen} alt="" />
-                        </div>
-                    ))}
-                    <i className="bx bxs-chevron-right-circle icon-perfil" onClick={() => cambiarPerfil("derecha")}></i>
+                    <Swiper
+                        breakpoints={{
+                            
+                            340: {
+                                slidesPerView: 2,
+                                spaceBetween: 15,
+                            },
+                            700: {
+                                slidesPerView: 3,
+                                spaceBetween: 15,
+                            },
+                        }}
+                        freeMode={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[FreeMode, Pagination]}>
+                        {perfilesSecundarios.map((perfil, index) => (
+                            <SwiperSlide className="swiper" key={index}>
+                                <div
+                                    className={`sec-perfil ${perfilSecundarioSeleccionado === index ? "selected" : ""}`}
+                                    onClick={() => {
+                                        cambiarPerfil(index);
+                                        setPerfilSecundarioSeleccionado(index);
+                                    }}>
+                                    <img className="img-sec" src={perfil.imagen} alt="" />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
