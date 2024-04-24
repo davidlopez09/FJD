3c7eadc84d6e0cdf12f6e41e506b4428113c3517
luis alvarez <alvarezanaya473@gmail.com>
2024-04-24 15:21:57 -0500

    jjhc

--- src/components/contacto.jsx
+++ src/components/contacto.jsx
@@ -81,7 +81,7 @@ export function Contactanos() {
                     height="250"
                     src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3943.185471982326!2d-75.890436!3d8.768614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwNDYnMDcuMCJOIDc1wrA1MycyNS42Ilc!5e0!3m2!1ses!2sco!4v1713296345732!5m2!1ses!2sco"></iframe>
 
-                <p className="parrafo-contacto">Cuentanos en que podemos ayudarte hoy</p>
+                <p className="parrafo-contacto">¡Cuéntanos en que podemos ayudarte hoy!</p>
             </div>
             <div className="conten">
                 <label className="label">
