48898ee875006085d01557c058685c90cd3cc69e
Carlos Tirado <mariotiradotovar@gmail.com>
2024-05-14 14:56:01 -0500

    cambio de imagenes

--- src/components/servicios.jsx
+++ src/components/servicios.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import imagen from "../assets/images/imagen.jpg";
-import imagen1 from "../assets/images/imagen1.webp";
-import imagen2 from "../assets/images/imagene2.webp";
-import imagen3 from "../assets/images/imagen3.webp";
-import imagen4 from "../assets/images/imagene4.webp";
+import imagen from "../assets/images/imagen.png";
+import imagen1 from "../assets/images/imagen1.png";
+import imagen2 from "../assets/images/imagene2.png";
+import imagen3 from "../assets/images/imagen3.png";
+import imagen4 from "../assets/images/imagen4.png";
 import "../assets/css/servicios.css";
 
 export function Servicios() {
@@ -24,7 +24,7 @@ export function Servicios() {
                 <div className={`card ${flippedCards[0] ? "flipped" : ""}`} onClick={() => handleCardClick(0)}>
                     <div className="face front">
                         <img src={imagen} alt="imagen" />
-                        <h3>Desarrollo de software</h3>
+                        <h3 className="texto">Desarrollo de software</h3>
                     </div>
                     <div className="face back">
                         <h3>Desarrollo de software</h3>
@@ -41,7 +41,7 @@ export function Servicios() {
                 <div className={`card ${flippedCards[1] ? "flipped" : ""}`} onClick={() => handleCardClick(1)}>
                     <div className="face front">
                         <img src={imagen1} alt="imagen1" />
-                        <h3>Desarrollo de aplicaciones web</h3>
+                        <h3 className="texto">Desarrollo de aplicaciones web</h3>
                     </div>
                     <div className="face back">
                         <h3>Desarrollo de aplicaciones web</h3>
@@ -58,7 +58,7 @@ export function Servicios() {
                 <div className={`card ${flippedCards[2] ? "flipped" : ""}`} onClick={() => handleCardClick(2)}>
                     <div className="face front">
                         <img src={imagen2} alt="imagen2" />
-                        <h3>Desarrollo de aplicaciones móviles</h3>
+                        <h3 className="texto">Desarrollo de aplicaciones móviles</h3>
                     </div>
                     <div className="face back">
                         <h3>Desarrollo de aplicaciones móviles</h3>
@@ -75,7 +75,7 @@ export function Servicios() {
                 <div className={`card ${flippedCards[3] ? "flipped" : ""}`} onClick={() => handleCardClick(3)}>
                     <div className="face front">
                         <img src={imagen3} alt="imagen3" />
-                        <h3>Expertos en desarrollo API</h3>
+                        <h3 className="texto">Expertos en desarrollo API</h3>
                     </div>
                     <div className="face back">
                         <h3>Expertos en desarrollo API</h3>
@@ -91,14 +91,16 @@ export function Servicios() {
 
                 <div className={`card ${flippedCards[4] ? "flipped" : ""}`} onClick={() => handleCardClick(4)}>
                     <div className="face front">
-                        <img src={imagen4} alt="imagen4" />
-                        <h3>Desarrollo de código seguro</h3>
+                        <img src={imagen4} alt="imagen4"/>
+                        <h3 className="texto">Desarrollo de código seguro</h3>
                     </div>
                     <div className="face back">
                         <h3>Desarrollo de código seguro</h3>
                         <p>Nuestra especialidad es el desarrollo de código web seguro y confiable.</p>
                         <div className="link">
-                            <a href="https://www.omatech.com/blog/2022/11/30/buenas-practicas-para-un-desarrollo-seguro/#:~:text=El%20desarrollo%20seguro%20es%20la,con%20distintos%20niveles%20de%20complejidad.">Ver más</a>
+                            <a href="https://www.omatech.com/blog/2022/11/30/buenas-practicas-para-un-desarrollo-seguro/#:~:text=El%20desarrollo%20seguro%20es%20la,con%20distintos%20niveles%20de%20complejidad.">
+                                Ver más
+                            </a>
                         </div>
                     </div>
                 </div>
